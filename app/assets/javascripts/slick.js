7c567dd7f50e03f7a9b9a26d86f5a527bda05422
Norman Chan <slowmanchan@gmail.com>
2016-09-06 14:44:27 -0400

    added slick gem, dropped slick cdn links

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,11 +1,10 @@
 
-$(document).ready(function(){
+$(document).on('ready', function(){
   $('.slider').slick({
     dots: true,
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear',
-
   });
 });
